8a3461c94db9f6d33f5ea930084350089717db57
doerodney <doerodney@gmail.com>
2015-12-25 12:16:13 -0800

    Broke out Parameter and Text functions into libraries.

---
+++ aws/cloudformation/powershell/LibParameter.ps1
@@ -0,0 +1,98 @@
+﻿Set-StrictMode -Version Latest
+
+. .\LibText.ps1
+
+function Add-Parameter
+{
+    [CmdletBinding()]
+    
+    param(
+        [hashtable] $Template,
+
+        [hashtable] $Parameter
+    )
+
+    # Template is a hash with a Parameters key
+    # The value of the Parameters key is, you guessed it, a hash.
+    # key = parameter name,
+    # value = parameter object
+    $txtName = Get-TextName
+    $txtParameters = Get-TextParameters
+
+    $name = $Parameter[(Get-TextName)]
+    $propertySet = $Parameter[(Get-TextPropertySet)]
+    
+    $hashParams = $Template[$txtParameters]
+    # TODO:  Complain if name already exists.
+    $hashParams[$name] = $propertySet   
+}
+
+
+function New-Parameter
+{
+    [CmdletBinding()]
+    param( 
+        
+        [Parameter(Mandatory=$true)]
+        [string] $Name,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('String', 
+            'Number', 
+            'List<Number>', 
+            'CommaDelimitedList', 
+            'AWS::EC2::AvailabilityZone::Name',
+            'AWS::EC2::Image::Id',
+            'AWS::EC2::Instance::Id',
+            'AWS::EC2::KeyPair::KeyName',
+            'AWS::EC2::SecurityGroup::GroupName',
+            'AWS::EC2::SecurityGroup::Id',
+            'AWS::EC2::Subnet::Id',
+            'AWS::EC2::Volume::Id',
+            'AWS::EC2::VPC::Id',
+            'AWS::Route53::HostedZone::Id',
+            'List<AWS::EC2::AvailabilityZone::Name>',
+            'List<AWS::EC2::Image::Id>',
+            'List<AWS::EC2::Instance::Id>',
+            'List<AWS::EC2::SecurityGroup::GroupName>',
+            'List<AWS::EC2::SecurityGroup::Id>',
+            'List<AWS::EC2::Subnet::Id>',
+            'List<AWS::EC2::Volume::Id>',
+            'List<AWS::EC2::VPC::Id>',
+            'List<AWS::Route53::HostedZone::Id>')]
+        [string] $Type = 'String',
+
+        [string] $Default = $null,
+        [switch] $NoEcho,
+        [string[]] $AllowedValues = $null,
+        [string] $AllowedPattern = $null,
+        [int] $MaxLength = $null,
+        [int] $MinLength = $null,
+        [string] $Description = $null,
+        [string] $ConstraintDescription = $null    
+    )
+
+    $txtType = Get-TextType
+
+    $hash = @{
+        $txtType = $Type  
+    }
+
+    if ($Default) { $hash['Default'] = $Default }
+    if ($NoEcho) { $hash['NoEcho'] = $true }
+    if ($AllowedValues.Count -gt 0) { $hash['AllowedValues'] = @($AllowedValues) }
+    if ($AllowedPattern) { $hash['AllowedPattern'] = $AllowedPattern }
+    if ($MaxLength) { $hash['MaxLength'] = $MaxLength }
+    if ($MinLength) { $hash['MinLength'] = $MinLength }
+    if ($Description) {$hash['Description'] = $Description }
+    if ($ConstraintDescription) {
+        $hash['ConstraintDescription'] = $ConstraintDescription 
+    }
+
+    $txtName = Get-TextName
+    $txtPropertySet = Get-TextPropertySet
+
+    $parameter = @{ $txtName = $Name; $txtPropertySet = $hash }
+
+    $parameter
+}
---
+++ aws/cloudformation/powershell/LibText.ps1
@@ -0,0 +1,14 @@
+﻿Set-StrictMode -Version Latest
+
+
+function Get-TextAWSTemplateFormatVersion { 'AWSTemplateFormatVersion' }
+function Get-TextConditions { 'Conditions' }
+function Get-TextDescription { 'Description' } 
+function Get-TextMappings { 'Mappings' }
+function Get-TextMetadata { 'Metadata' }
+function Get-TextName { 'Name' }
+function Get-TextOutputs { 'Outputs' }
+function Get-TextParameters { 'Parameters' }
+function Get-TextPropertySet { 'PropertySet' }
+function Get-TextResources { 'Resources' }
+function Get-TextType{ 'Type' }
